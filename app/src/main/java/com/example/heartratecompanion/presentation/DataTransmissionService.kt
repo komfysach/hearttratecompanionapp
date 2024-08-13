257645585d54ea80c1a82a4ec3b9608f48ad3c34
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-08-13 17:02:52 +0200

    update data transmission service

--- app/src/main/java/com/example/heartratecompanion/presentation/DataTransmissionService.kt
+++ app/src/main/java/com/example/heartratecompanion/presentation/DataTransmissionService.kt
@@ -1,12 +1,17 @@
 package com.example.heartratecompanion.presentation
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,36 +31,40 @@ class DataTransmissionService(private val context: Context) : Service() {
         private const val TAG = "DataTransmissionService"
         private const val SMARTPHONE_MAC_ADDRESS = "54:92:09:AB:9B:F4" // Replace with your smartphone's MAC address
         private val MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB") // Replace with your app's UUID
+        const val ACTION_CONNECTED_STATUS = "com.example.heartratecompanion.ACTION_CONNECTED_STATUS"
+        const val EXTRA_CONNECTED = "com.example.heartratecompanion.EXTRA_CONNECTED"
     }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
+    @SuppressLint("MissingPermission")
     fun connectToSmartphone() {
         CoroutineScope(Dispatchers.IO).launch {
-            var retryCount = 0
-            val maxRetries = 3 // Adjust as needed
-            while (retryCount < maxRetries && bluetoothSocket?.isConnected != true) {
-                try {
-                    val smartphoneDevice: BluetoothDevice = bluetoothAdapter?.getRemoteDevice(SMARTPHONE_MAC_ADDRESS)!!
-                    bluetoothSocket = smartphoneDevice.createRfcommSocketToServiceRecord(MY_UUID)
-                    bluetoothSocket?.connect()
-                    Log.d(TAG, "Connected to smartphone")
-                } catch (e: IOException) {
-                    Log.e(TAG, "Error connecting to smartphone: ${e.message}")
-                    retryCount++
-                    delay(1000) // Wait 1 second before retrying
+            try {
+                val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+                if (pairedDevices != null) {
+                    for (device in pairedDevices) {
+                        Log.d(TAG, "Checking device: ${device.name}")
+                        if (device.name == "HUAWEI P30 Pro") { // Replace with your smartphone's name
+
+                            bluetoothSocket = device.createRfcommSocketToServiceRecord(MY_UUID)
+                            bluetoothSocket?.connect()
+
+                            Log.d(TAG, "Connected to ${device.name}")
+                            updateConnectionStatus(true)
+                            break
+                        }
+                    }
                 }
-            }
-            if (bluetoothSocket?.isConnected != true) {
-                Log.e(TAG, "Failed to connect to smartphone after $maxRetries retries")
-                // Handle connection failure (e.g., notify user)
+            } catch (e: IOException) {
+                Log.e(TAG, "Connection to smartphone failed", e)
             }
         }
     }
 
-    fun sendHeartRateData(heartRate: Float) {
+    suspend fun sendHeartRateData(heartRate: Float) {
         if (bluetoothSocket?.isConnected == true) {
             dataTransferJob = CoroutineScope(Dispatchers.IO).launch {
                 while (true) {
@@ -76,7 +85,8 @@ class DataTransmissionService(private val context: Context) : Service() {
             // Attempt to reconnect
             connectToSmartphone()
             // You might want to add a delay here before checking the connection again
-            if (bluetoothSocket?.isConnected== true) {
+            delay(1000) // Wait 1 second before checking the connection again
+            if (bluetoothSocket?.isConnected == true) {
                 // Retry sending data if the connection was successful
                 sendHeartRateData(heartRate)
             } else {
@@ -91,6 +101,7 @@ class DataTransmissionService(private val context: Context) : Service() {
         try {
             bluetoothSocket?.close()
             Log.d(TAG, "Connection closed")
+            updateConnectionStatus(false)
         } catch (e: IOException) {
             Log.e(TAG, "Error closing connection: ${e.message}")
         }
@@ -100,4 +111,16 @@ class DataTransmissionService(private val context: Context) : Service() {
         super.onDestroy()
         stopDataTransmission()
     }
+
+    private fun updateConnectionStatus(isConnected: Boolean) {
+        val intent = Intent(ACTION_CONNECTED_STATUS).apply {
+            putExtra(EXTRA_CONNECTED, isConnected)
+        }
+        if (context != null) {
+            Handler(Looper.getMainLooper()).post {
+                context.applicationContext.sendBroadcast(intent)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/heartratecompanion/presentation/MainActivity.kt
+++ app/src/main/java/com/example/heartratecompanion/presentation/MainActivity.kt
@@ -1,44 +1,68 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package com.example.heartratecompanion.presentation
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private lateinit var heartRateDataService: HeartRateDataService
     private lateinit var dataTransmissionService: DataTransmissionService
 
+    private var isConnected by mutableStateOf(false) // Make isConnected accessible
+
+    private val connectionStatusReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == DataTransmissionService.ACTION_CONNECTED_STATUS) {
+                isConnected = intent.getBooleanExtra(DataTransmissionService.EXTRA_CONNECTED, false)
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Initialize heart rate data service
         heartRateDataService = HeartRateDataService(this)
         dataTransmissionService = DataTransmissionService(this)
-        dataTransmissionService.connectToSmartphone()
+        connectionStatusReceiver.onReceive(
+            this,
+            Intent(DataTransmissionService.ACTION_CONNECTED_STATUS).apply {
+                putExtra(DataTransmissionService.EXTRA_CONNECTED, isConnected)
+            }
+        )
 
         installSplashScreen()
 
@@ -51,8 +75,6 @@ class MainActivity : ComponentActivity() {
             startService(intent)
         }
         dataTransmissionService = DataTransmissionService(this)
-        dataTransmissionService?.connectToSmartphone()
-
 
         setContent {
 
@@ -60,52 +82,83 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(connectionStatusReceiver, IntentFilter(DataTransmissionService.ACTION_CONNECTED_STATUS),
+            RECEIVER_NOT_EXPORTED
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(connectionStatusReceiver)
+    }
+
     @Composable
     fun HeartRateRecordingScreen() {
-        var isRecording by remember {
-            mutableStateOf(false)
-        }
-        var heartRate by remember {
-            mutableStateOf(0f)
-        }
+        var isRecording by remember { mutableStateOf(false) }
+        var heartRate by remember { mutableStateOf(0f) }
 
         heartRateDataService.setOnHeartRateDataListener { newHeartRate ->
             heartRate = newHeartRate
         }
 
-        if (isRecording) {
-//            dataTransmissionService.sendHeartRateData(heartRate)
-        }
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            if (isRecording) {
-                Text(text = "Current Heart Rate: $heartRate BPM")
-                CircularProgressIndicator()
-                // Stop recording
-                Button(onClick = { isRecording = false
-                    heartRateDataService.stopHeartRateMonitoring()
-                    dataTransmissionService.stopDataTransmission()
-                    // Initiate data collection to send to smartphone
-                }) {
-                    Text(text = "Stop")
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                if (isRecording && isConnected) {
+                    Text(text = "Heart Rate: $heartRate")
+                    CircularProgressIndicator()
+                    // Stop recording
+                    Button(onClick = {
+                        isRecording = false
+                        heartRateDataService.stopHeartRateMonitoring()
+                        dataTransmissionService.stopDataTransmission()
+                    }) {
+                        Text(text = "Stop")
+                    }
+                    LaunchedEffect(key1 = isRecording) { // Only launch when isRecording changes
+                        if (isRecording) {
+                            snapshotFlow { heartRate }
+                                .collect { hr ->
+                                    dataTransmissionService.sendHeartRateData(hr)
+                                }
+                        }
+                    }
                 }
-            } else {
-                // Start recording
-                Button(onClick = { isRecording = true
-                    heartRateDataService.startHeartRateMonitoring()
-                    dataTransmissionService.sendHeartRateData(heartRate)
-                    // Initiate data collection to send to smartphone
-                }) {
-                    Text(text = "Start")
-                }
+                else if (!isRecording && isConnected) { // Check connection status
+                    Text(text = "Connected to Smartphone")
+                    // Start recording
+                    Button(onClick = {
+                        isRecording = true
+                        heartRateDataService.startHeartRateMonitoring()
 
+                    }) {
+                        Text(text = "Start")
+                    }
+                } else {
+                    Text(text = "Not connected to Smartphone")
+                    // Connect button
+                    Button(onClick = {
+                        // Call a function to initiate connection to the smartphone
+                         dataTransmissionService.connectToSmartphone()
+                    },
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = MaterialTheme.colors.onPrimary
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+
+                        Text(text = "Connect")
+                    }
+                }
             }
 
-        }
     }
 
 
